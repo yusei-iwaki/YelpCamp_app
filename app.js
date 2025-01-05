0d2af782346fe5901146b6501bd776ff8c76f311
yusei-iwaki <iwaki.yusei11@gmail.com>
2025-01-06 00:21:48 +0900

    機能追加：キャンプ場詳細

--- app.js
+++ app.js
@@ -26,6 +26,11 @@ app.get('/campgrounds', async(req, res) => {
     res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+});
+
 app.listen(3000, () => {
     console.log('ポート3000でサーバーを起動しました');
 });
