c40e1a789d48309ad59545e5456798ae812dc210
yusei-iwaki <iwaki.yusei11@gmail.com>
2025-01-06 00:05:10 +0900

    seeds新規作成

---
+++ seeds/index.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp',
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => {
+        console.log('MongoDBコネクションOK！！');
+    })
+    .catch(err => {
+        console.log('MongoDBコネクションエラー！！')
+    });
+
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const randomCityIndex = Math.floor(Math.random() * cities.length);
+        const camp = new Campground({
+            location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
+            title: `${sample(descriptors)}・${sample(places)}`
+        });
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
