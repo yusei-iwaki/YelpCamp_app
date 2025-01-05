0911bf190f26ab6fd76200164bb1140c303c6ce7
yusei-iwaki <iwaki.yusei11@gmail.com>
2025-01-06 00:40:02 +0900

    機能追加：キャンプ場新規登録

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -26,10 +28,20 @@ app.get('/campgrounds', async(req, res) => {
     res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/new', async(req, res) => {
+    res.render('campgrounds/new');
+});
+
 app.get('/campgrounds/:id', async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
 });
+
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+})
 
 app.listen(3000, () => {
     console.log('ポート3000でサーバーを起動しました');
