e7c3da67540b71f211424ff07deb63917f162f54
yusei-iwaki <iwaki.yusei11@gmail.com>
2025-01-06 00:13:08 +0900

    機能追加：キャンプ場一覧

--- app.js
+++ app.js
@@ -21,10 +21,9 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/makecampground', async(req, res) => {
-    const camp = new Campground({ title: '私の庭', description: '気軽に安くキャンプ！' });
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async(req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
 });
 
 app.listen(3000, () => {
